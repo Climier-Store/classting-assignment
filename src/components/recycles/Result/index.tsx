0a2c6ad327295e9886d6a80855f9643c271c27b2
Climier-Store <jongyeon.dev@gmail.com>
2022-07-09 17:32:39 +0900

    feat: add chart in Result

--- src/components/recycles/Result/index.tsx
+++ src/components/recycles/Result/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { RadialChart } from 'react-vis';
 import { ResultWrapper } from './styled';
 
 interface Props {
@@ -10,8 +11,17 @@ interface Props {
 function Result(props: Props) {
   const { time, successCount, onRetry } = props;
 
+  const chartData = React.useMemo(() => {
+    return [
+      { angle: successCount, label: '정답' },
+      { angle: 10 - successCount, label: '오답' },
+    ];
+  }, [successCount]);
+
   return (
     <ResultWrapper>
+      <RadialChart data={chartData} width={300} height={300} showLabels />
+
       <p>퀴즈를 마칠 때까지 소요된 시간: {time}</p>
       <p>정답 개수: {successCount}</p>
       <p>오답 수: {10 - successCount}</p>
